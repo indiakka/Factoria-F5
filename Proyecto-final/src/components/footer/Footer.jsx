b95fe33f1e30b773187083c85bc96a714938472e
indiakka <78439130+indiakka@user.noreply.git>
2024-03-13 19:35:48 +0100

    cambiar estética footer

--- Proyecto-final/src/components/footer/Footer.jsx
+++ Proyecto-final/src/components/footer/Footer.jsx
@@ -1,48 +1,40 @@
 import './footer.css';
 import { NavLink } from "react-router-dom";
 
-const Footer = () => {
+const Footer = () =>
+{
   return (
-    <footer>
-
+    <footer className='footer'>
       <div className='footer-primer-bloque'>
-          <img className="footer-logo" src="src/assets/images/logoblanco.png" alt="Logo" />
-          <h3 className='footer-naming-solo-mobile'>Adopta amor</h3>
-          <p>C/ de la Diputació, 150 <br></br>
-          L'Eixample, 08007 Barcelona <br></br>
-          +34611234567</p>
+        <img className="footer-logo" src="src/assets/images/logoblanco.png" alt="Logo" />
+        <h3 className='footer-naming-solo-mobile'>Adopta amor</h3>
       </div>
-
       <div className='footer-segundo-bloque'>
-
         <div className='footer-nav-descubrir'>
           <h3>Descubrir</h3>
-          <ul>
-            <li>Cómo funciona</li>
-            <li><NavLink to='/Adoptar'>Adopta</NavLink></li>
-            <li><NavLink to='/Donar'>Dona</NavLink></li>
-          </ul>
+          <p className='efecto-rosa'>Cómo funciona</p>
+          <p className='efecto-rosa'><NavLink to='/Adoptar'>Adopta</NavLink></p>
+          <p className='efecto-rosa'><NavLink to='/Donar'>Dona</NavLink></p>
         </div>
-
         <div className='footer-nav-info'>
           <h3>Info</h3>
-          <ul>
-            <li>Sobre nosotros</li>
-            <li>FAQ</li>
-            <li>Contactar</li>
-          </ul>
+          <p className='efecto-rosa'>Sobre nosotros</p>
+          <p className='efecto-rosa'>FAQ</p>
+          <p className='efecto-rosa'>Contactar</p>
         </div>
-
         <div className='footer-redes-sociales'>
-          <img src="src/assets/images/instagram.svg" alt="instagram-logo" />
-          <img src="src/assets/images/facebook.svg" alt="facebook-logo" />
-          <img src="src/assets/images/twitterx.svg" alt="twitter-logo" />
+          <h3>
+            <NavLink><img src="src/assets/images/instagram.svg" alt="instagram-logo" /></NavLink>
+            <NavLink><img src="src/assets/images/facebook.svg" alt="facebook-logo" /></NavLink>
+            <NavLink><img src="src/assets/images/twitterx.svg" alt="twitter-logo" /></NavLink>
+          </h3>
+          <p>C/ de la Diputació, 150 <br></br>
+            L'Eixample, 08007 Barcelona <br></br>
+            +346112345670</p>
         </div>
-
       </div>
-
     </footer>
   )
 }
 
-export default Footer
+export default Footer;
