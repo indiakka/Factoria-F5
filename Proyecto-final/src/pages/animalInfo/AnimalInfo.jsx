9849fc0e778357b3f715b5832887ec3c84007b42
indiakka <78439130+indiakka@user.noreply.git>
2024-03-16 18:08:53 +0100

    funcionalidad de botones en animalInfo funcionando

--- Proyecto-final/src/pages/animalInfo/AnimalInfo.jsx
+++ Proyecto-final/src/pages/animalInfo/AnimalInfo.jsx
@@ -1,11 +1,60 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { useParams, useNavigate } from "react-router";
 import "./animalInfo.css";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faTimes, faPaw, faEdit, faTrash } from '@fortawesome/free-solid-svg-icons';
+import { faTimes, faPaw } from '@fortawesome/free-solid-svg-icons';
+import { NavLink } from "react-router-dom";
+
 
 const AnimalInfo = ( { animal } ) =>
 {
+  const [ estadoAnimal, setEstadoAnimal ] = useState( [] );
+  const { id } = useParams();
+  const [ animalesCasita, setAnimalesCasita ] = useState( [] );
+  const navigate = useNavigate()
   const [ isOpen, setIsOpen ] = useState( true ); // Estado para controlar la visibilidad de la tarjeta
+
+  useEffect( () =>
+  {
+    const animalInfo = async () =>
+    {
+      const response = await axios.get( `http://localhost:3000/results/${id}` );
+      setEstadoAnimal( response.data );
+    };
+    animalInfo();
+  }, [ id ] );
+
+  const anadirAnimal = () =>
+  {
+    const listadoAnimales = [ ...animalesCasita, { ...animal, id: animal.id } ];
+    setAnimalesCasita( listadoAnimales );
+    alert( "Animal añadido a tu casita" )
+  };
+
+  useEffect( () =>
+  {
+    const animalesAlmacenados = localStorage.getItem( 'animalesCasita' );
+    if ( animalesAlmacenados )
+    {
+      setAnimalesCasita( JSON.parse( animalesAlmacenados ) );
+    }
+  }, [] );
+
+  useEffect( () =>
+  {
+    localStorage.setItem( 'animalesCasita', JSON.stringify( animalesCasita ) );
+  }, [ animalesCasita ] );
+
+  const handleSubmit = async ( id ) =>
+  {
+    const conf = window.confirm( '¿Quieres realmente borrar este animal?' )
+    if ( conf )
+    {
+      await axios.delete( `http://localhost:3000/results/${id}` )
+      alert( 'Este animal ha sido borrado correctamente' )
+      navigate( '/adoptar' )
+    }
+  }
 
   const handleClose = () =>
   {
@@ -38,7 +87,7 @@ const AnimalInfo = ( { animal } ) =>
           <p>Años: {animal.años}</p>
           <p>Gastos de Gestión: {animal.gastosDeGestion}</p>
           <div className="container--button">
-            <button className="button-adopta btn--conoceme">
+            <button onClick={anadirAnimal} className="button-adopta btn--conoceme">
               <FontAwesomeIcon icon={faPaw} /> Conóceme
             </button>
           </div>
@@ -47,10 +96,11 @@ const AnimalInfo = ( { animal } ) =>
           <div className='contenedor-cerrar-card' onClick={handleClose}>
             <FontAwesomeIcon icon={faTimes} className='cerrar-card' />
           </div>
-          <button onClick={handleClose} className="botones--editar">
-            <img src="../src/assets/images/Edit.png" alt="editar" />
-          </button>
-          <button onClick={handleClose} className="botones--editar">
+          <NavLink to={`/editarInfo/${animal.id}`}>
+            <button onClick={handleClose} className="botones--editar">
+              <img src="../src/assets/images/Edit.png" alt="editar" /></button>
+          </NavLink>
+          <button onClick={() => { handleSubmit( animal.id ); handleClose(); }} className="botones--editar">
             <img src="../src/assets/images/Delete.png" alt="borrar" />
           </button>
         </div>
